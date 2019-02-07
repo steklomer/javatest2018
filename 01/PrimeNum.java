c3205750cedfef6f8f5e8cd06b534d8745c7a8f4
steklomer <dekans@mail.ru>
2019-02-07 20:25:31 +0300

    Create PrimeNum.java
    Задание 1
    Необходимо написать программу, которая вычисляет простые числа в пределах от 1
    до N. N вводится вручную во время выполнения программы.

---
+++ 01/PrimeNum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNum {
+  
+  public static void main(String args[]) {
+    System.out.println("¬ведите положительное число:");
+    Scanner in = new Scanner(System.in);
+    int maxNum = in.nextInt();
+    System.out.println("–езультат:");
+    for (int i = 1; i <= maxNum; i++) {
+      if (isPrime(i)) {          
+        System.out.println(i);
+      }
+    }
+  }
+
+  public static boolean isPrime(int n) {  
+    if (n == 1) 
+      return false;
+    for (int j=2; j < n; j++) {
+      if (n % j == 0)
+        return false;
+    }
+    return true;    
+  }
+}

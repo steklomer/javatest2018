e35d8375ff41aef7ddbc7e7d7269682771ccbdb2
steklomer <dekans@mail.ru>
2019-02-07 20:32:04 +0300

    Create Palindrome.java
    Задание 5
    Необходимо написать программу, которая проверяет слово на "палиндромность".
    Слово для проверки вводится вручную во время выполнения программы.

---
+++ 05/Palindrome.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Palindrome {
+  
+  public static void main(String args[]) {
+    System.out.println("Введите слово:");
+    Scanner in = new Scanner(System.in);
+    String sWord = in.next();
+    int len = sWord.length();
+    char[] cArr = new char[len];
+    char[] revArr = new char[len];
+    for (int i = 0; i < len; i++) {
+      cArr[i] = sWord.charAt(i);
+      revArr[i] = sWord.charAt(len - 1 - i);
+    }
+    //System.out.println("Массив: " + Arrays.toString(cArr));
+    //System.out.println("Обратный массив: " + Arrays.toString(revArr));
+    System.out.println("Палиндром: " + Boolean.toString(Arrays.equals(cArr, revArr)));
+  }
+  
+  //public static char[] reverse(char[] inArr){
+  //  int arrLen = inArr.length;
+  //  for (int i = 0; i < arrLen / 2; i++) {
+  //    char arrElem = inArr[i];
+  //    inArr[i] = inArr[(arrLen-1)-i];
+  //    inArr[(arrLen-1)-i] = arrElem;
+  //  }
+  //  return inArr;
+  //}
+}

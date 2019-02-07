e756d32464c56d36bd13dc18a9995d8cb3c49b1b
steklomer <dekans@mail.ru>
2019-02-07 21:17:48 +0300

    compile and test

--- 04/CheckBoolean.java
+++ 04/CheckBoolean.java
@@ -6,13 +6,13 @@ public class CheckBoolean {
     int ElemCount = 4;
     int TrueCount = 2;
     boolean[] bArr = new boolean[ElemCount];
-    System.out.println("Введите " + ElemCount + " логических значений");
+    System.out.println("Enter " + ElemCount + " logical values (true/false)");
     for (int i = 0; i < ElemCount; i++) {
       System.out.println(i+1 +":");
       Scanner in = new Scanner(System.in);
       bArr[i] = in.nextBoolean();
     }
-    System.out.println("Массив:" + Arrays.toString(bArr));
+    System.out.println("Array: " + Arrays.toString(bArr));
     int cnt = 0;
     for (int i = 0; i < ElemCount; i++) {
       if (bArr[i])
@@ -20,7 +20,7 @@ public class CheckBoolean {
       if (cnt > TrueCount)
         break;
     }
-    System.out.println("Результат:" + Boolean.toString(cnt == TrueCount));    
+    System.out.println("Has only " + TrueCount + " true values? " + Boolean.toString(cnt == TrueCount));    
   
   }
 }

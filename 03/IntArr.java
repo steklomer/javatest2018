945b3496e77dfe6749fd7689fa788e00d120e711
steklomer <dekans@mail.ru>
2019-02-07 21:11:54 +0300

    compile and test

--- 03/IntArr.java
+++ 03/IntArr.java
@@ -4,7 +4,7 @@ public class IntArr {
 
   public static void main(String args[]) {
 	Scanner in = new Scanner(System.in);
-  System.out.println("Введите массив целых чисел через пробел:");
+  System.out.println("Enter integer array separated with space (enter letter to stop):");
 	ArrayList<Integer> list = new ArrayList<Integer>();
 	while (in.hasNextInt()) {
 	  list.add(in.nextInt());
@@ -12,13 +12,12 @@ public class IntArr {
 	int[] iArr = list.stream().mapToInt(i->i).toArray();
 	Arrays.sort(iArr);
 	int arrLen = iArr.length;
-	System.out.println(arrLen);
+	// System.out.println(arrLen);
 	for (int i = 0; i < arrLen / 2; i++) {
 	  int arrElem = iArr[i];
 	  iArr[i] = iArr[(arrLen-1)-i];
 	  iArr[(arrLen-1)-i] = arrElem;
 	}
-	System.out.println("Результат:");
-	System.out.println(Arrays.toString(iArr));
+	System.out.println("Result: " + Arrays.toString(iArr));
   }
 }
